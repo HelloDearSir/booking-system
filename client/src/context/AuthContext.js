f0c86ce8da08ecc47fdcb5f4c04fedd02114d6c9
HelloDearSir <jonbarton@hotmail.co.uk>
2021-05-31 22:06:19 +0100

    Update AuthContext.js

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -7,7 +7,7 @@ function AuthContextProvider(props) {
   const [loggedIn, setLoggedIn] = useState(undefined);
 
   async function getLoggedIn() {
-     const loggedInRes = await axios.get("http://localhost:50000/auth/loggedIn");
+     const loggedInRes = await axios.get("/auth/loggedIn");
  
     setLoggedIn(loggedInRes.data);
   }
