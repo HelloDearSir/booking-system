450f3340ebf75f2c0d3d66356fd2e51382923705
HelloDearSir <jonbarton@hotmail.co.uk>
2021-08-12 16:07:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,55 +18,7 @@ app.use(
 );
 const Booking = require("./models/Booking");
 mongoose.connect('mongodb+srv://Jon:Password@cluster0.mipjc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-const port = process.env.PORT || 5000;
-const Blogs  = new mongoose.Schema({
-    title: String,
-    body: String
-  })
-  
-
-
-
-
-  const newBlog = mongoose.model('newBlogs', Blogs);
-  module.exports = newBlog;
-  app.post('/save', function (req, res) {
-    console.log(req.body)
-    const data = req.body;
-    const newBlogs =  newBlog(data);
-    newBlogs.save((err) => {
-    if(err) console.error(err);
-   
-   
-      else  { 
-        newBlog.find({}).exec(function (err, results) {
-          if (err) {
-            console.error(err);
-          }
-          if (results) {
-            tests = results
-        
-          }
-        })
-        
-  
-  res.json({ msg: "we got the daata"})
-  
-  
-      }
-      
-    })
-  });
-newBlog.find({}).exec(function (err, results) {
-    if (err) {
-      console.error(err);
-    }
-    if (results) {
-      tests = results
-  
-    }
-  })
-  
+const port = process.env.PORT || 5000;   
   
   app.get('/todo', function(req,res){
     res.send({tests})
@@ -83,31 +35,8 @@ newBlog.find({}).exec(function (err, results) {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
   }
-
-  
-  newBlog.find({}).exec(function (err, results) {
-    if (err) {
-      console.error(err);
-    }
-    if (results) {
-      tests = results
-  
-    }
-  })
-  
-  
-  var Testing
  
-
-
-
-
-
-
-
-
-
-
+   
 app.listen(port, function() {
     console.log("express is running");
 })
